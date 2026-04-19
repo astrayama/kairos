808d37d48eeeec8bdfe265f2a9f962b5eeaacba7
astrayama <isabelabonitalla@gmail.com>
2026-04-19 02:25:17 -0400

    Initial Commit

---
+++ kairos/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  kairos
+//
+//  Created by Isabel Abonitalla on 4/19/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ kairos/kairosApp.swift
@@ -0,0 +1,32 @@
+//
+//  kairosApp.swift
+//  kairos
+//
+//  Created by Isabel Abonitalla on 4/19/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct kairosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
